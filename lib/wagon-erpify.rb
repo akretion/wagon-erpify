771d0dc2906721c7af7ce8ef4dbf3e19b300ecd3
Raphaël Valyi <rvalyi@gmail.com>
2013-12-30 03:36:01 -0200

    pretty decent version: proper localization and use Rack properly wherever possible (Wagon could be improved to help us inject Ooor::Rack at the right position drectly

--- lib/wagon-erpify.rb
+++ lib/wagon-erpify.rb
@@ -2,14 +2,19 @@ require 'erpify'
 require 'locomotive/mounter'
 require 'locomotive/wagon/server'
 require 'ostruct'
-require 'uri'
+require 'cgi'
 require 'delegate'
+require 'ooor/rack'
 
 
 module Erpify
   module ContentEntryHelper
+    include Ooor::LocaleMapper
+
     def content_type
-      @content_type ||= OpenStruct.new(slug: self.class.param_key)
+      locale = Locomotive::Mounter.locale.to_s
+      context = {'lang' => to_erp_locale(locale)}
+      @content_type ||= OpenStruct.new(slug: self.class.param_key(context))
     end
 
     def content_entry
@@ -53,10 +58,7 @@ module Locomotive
         def content_type_with_erpify
           c_type = content_type_without_erpify
           if c_type && c_type.slug == 'ooor_entries'
-            param_key = (self.to_s.split('/')[0]).gsub('-', '.')
-            model = Ooor::Base.connection_handler.retrieve_connection(Ooor.default_config).const_get(param_key)
-            new_slug = model.alias.gsub('.', '-')
-            SlugDecorator.new(c_type, new_slug)
+            SlugDecorator.new(c_type, self.to_s.split('/')[0])
           else
             c_type
           end
@@ -64,7 +66,6 @@ module Locomotive
 
         alias_method :content_type_without_erpify, :content_type
         alias_method :content_type, :content_type_with_erpify
-
       end
     end
   end
@@ -73,45 +74,25 @@ end
 
 module Locomotive::Wagon
   class Server
-
-    class Locale < Middleware
-
-      def to_erp_locale(locale)
-        mappings = {'fr' => 'fr_FR', 'en' => 'en_US'}
-        mappings[locale] || locale
-      end
+    class Page < Middleware # sadly unlike Rails Wagon offers us no way to place our OOOR middleware at the right position, that's why we monkey patch instead
 
       def call_with_erpify(env)
-        self.set_accessors(env)
-        self.set_locale!(env)
-        #res = call_without_erpify(env) # we cannot call it now as it may blow up if OpenERP locale isn't set to properly match models from URL's
-        connec = Ooor::Base.connection_handler.retrieve_connection(Ooor.default_config)
-        connec.connection_session['lang'] = to_erp_locale(env['wagon.locale'].to_s)
-        app.call(env)
+        ooor_rack = Ooor::Rack.new(@app)
+        ooor_rack.set_ooor!(env)
+        erpify_assigns = {
+                          "ooor_public_model" => Erpify::Liquid::Drops::OoorPublicModel.new(),
+                          "ooor_model" => Erpify::Liquid::Drops::OoorPublicModel.new(), #no authentication in Wagon
+                        }
+        env["wagon.liquid_assigns"].merge!(erpify_assigns)
+        call_without_erpify(env)
       end
 
       alias_method :call_without_erpify, :call
       alias_method :call, :call_with_erpify
 
-    end
-
-
-    class Renderer < Middleware
-
-      def locomotive_context_with_erpify(other_assigns = {})
-        erpify_assigns = {
-                          "ooor_public_model" => Erpify::Liquid::Drops::OoorPublicModel.new(),
-                          "ooor_model" => Erpify::Liquid::Drops::OoorPublicModel.new(), #no authentication in Wagon
-                        }
-
-        context = locomotive_context_without_erpify(other_assigns)
-        context.merge(erpify_assigns)
-        context
+      def set_ooor_context!(env)
+        
       end
-
-      alias_method :locomotive_context_without_erpify, :locomotive_context
-      alias_method :locomotive_context, :locomotive_context_with_erpify
-
     end
 
 
@@ -123,15 +104,15 @@ module Locomotive::Wagon
 
         if page.content_type_without_erpify.slug == 'ooor_entries'
           method_or_key = self.path.split('/')[0].gsub('-', '.')
-          model = Ooor::Base.connection_handler.retrieve_connection(Ooor.default_config).const_get(method_or_key)
-          env['wagon.content_entry'] = model.find_by_param(URI.unescape(permalink)) #TODO implement find by permalink (and to_param)
-          return
-        end
-
-        if content_entry = self.page.content_type.find_entry(permalink)
-          env['wagon.content_entry'] = content_entry
+          lang = env['ooor']['ooor_context']['lang'] || 'en_US'
+          model = Ooor::Base.connection_handler.retrieve_connection(Ooor.default_config).const_get(method_or_key, lang)
+          env['wagon.content_entry'] = model.find_by_param(CGI::unescape(permalink))
         else
-          env['wagon.page'] = nil
+          if content_entry = self.page.content_type.find_entry(permalink)
+            env['wagon.content_entry'] = content_entry
+          else
+            env['wagon.page'] = nil
+          end
         end
       end
     end
@@ -145,6 +126,7 @@ module Locomotive
     module Liquid
       module Tags
         module PathHelper
+
           def retrieve_page_from_handle_with_erpify(context)
             mounting_point = context.registers[:mounting_point]
             context.scopes.reverse_each do |scope|
@@ -158,7 +140,6 @@ module Locomotive
 
           alias_method :retrieve_page_from_handle_without_erpify, :retrieve_page_from_handle
           alias_method :retrieve_page_from_handle, :retrieve_page_from_handle_with_erpify
-
         end
       end
     end
@@ -169,7 +150,7 @@ end
 begin
   config_file = "#{Dir.pwd}/config/ooor.yml"
   config = YAML.load_file(config_file)['development']
-  Ooor.default_config = HashWithIndifferentAccess.new(config)
+  Ooor.default_config = HashWithIndifferentAccess.new(config).merge(locale_rack_key: 'wagon.locale')
 rescue SystemCallError
   puts """failed to load OOOR yaml configuration file.
        make sure your app has a #{config_file} file correctly set up\n\n"""
